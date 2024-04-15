d0775f48eccd23026871fec295f059bd26e5b8c2
Dohwan Kim <87230563+ehghks021203@users.noreply.github.com>
2024-04-15 18:06:43 +0900

    Delete greenery-backend directory

--- greenery-backend/app/routes/__init__.py
+++
@@ -1,3 +0,0 @@
-from app import app
-from .classification import classification_routes
-from .chatbot import chatbot_routes
--- greenery-backend/run.py
+++
@@ -1,5 +0,0 @@
-from app import app
-from config import Server
-
-if __name__ == "__main__":
-    app.run(host=Server.SERVER_DOMAIN, port=Server.SERVER_PORT, debug=True)
