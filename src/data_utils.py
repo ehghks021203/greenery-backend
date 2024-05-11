bb476634d9257f33fff1c27879880094226f2df0
ehghks021203 <ehghks021203@gmail.com>
2024-05-11 11:18:16 +0900

    2024.05.11. Modify data_utils.py

--- src/data_utils.py
+++ src/data_utils.py
@@ -1,13 +1,7 @@
 import os
 import sys
 sys.path.append(os.path.dirname(os.path.abspath('')))
-import config
-import json
 import shutil
-import numpy as np
-from sklearn.model_selection import train_test_split
-from PIL import Image
-import cv2
 
 def list_dir(path: str) -> list:
     return [x for x in os.listdir(os.path.join(path))]
