6c11502704eb2c297606131687a118144b4893db
ehghks021203 <ehghks021203@gmail.com>
2024-05-11 11:48:18 +0900

    2024.05.11.

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,16 @@
 from flask import Flask
+from flask_mysqldb import MySQL
 from config import Config
 
 # create flask app
 app = Flask(__name__)
 app.config.from_object(Config)
 
+# initialize extensions
+mysql = MySQL(app)
+
 # 블루프린트 등록
-from app.routes import classification_routes, chatbot_routes
+from app.routes import classification_routes, chatbot_routes, get_trash_data_routes
 app.register_blueprint(classification_routes)
 app.register_blueprint(chatbot_routes)
+app.register_blueprint(get_trash_data_routes)
--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,3 +1,4 @@
 from app import app
 from .classification import classification_routes
 from .chatbot import chatbot_routes
+from .get_trash_data import get_trash_data_routes
