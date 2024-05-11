31fd2a2bc259678d615aa1989e907e14d660e95f
ehghks021203 <ehghks021203@gmail.com>
2024-05-11 11:44:13 +0900

    2024.05.11. Add get_trash_data.py

--- app/routes/chatbot.py
+++ app/routes/chatbot.py
@@ -33,12 +33,62 @@ def chat():
             "result": "error", 
             "msg": "missing json in request"
         }), 400
+    # 파라미터 받아오기
+    user_msg = request.json["msg"] if "msg" in request.json else ""
+    history = request.json["history"] if "history" in request.json != None else []
+
+    if user_msg == "undefined":
+        bot_msg = "물체가 인식되지 않았습니다. 사진을 다시 찍어주세요."
+        return jsonify({
+            "result": "success", 
+            "msg": bot_msg,
+            "history": history
+        }), 200
+
+    if len(history) == 0:
+        bot_msg, history = chatbot.rag(user_msg)
+    
+    else:
+        bot_msg, history = chatbot.chat(user_msg, history)
+    
+    return jsonify({
+        "result": "success", 
+        "msg": bot_msg.strip("'"),
+        "history": history
+    }), 200
+
+@chatbot_routes.route("/chat_rag", methods=["POST"])
+def chat_rag():
+    """챗봇 기억력 테스트 함수
+
+    ** 현재 아이디만 안다면 데이터를 수정할 수 있는 상태로, 수정이 필요
+
+    Params:
+        msg `str`:
+            사용자가 보낸 채팅 내용
+        history `list`:
+            대화 기록
+    
+    Returns:
+        result `str`:
+            응답 성공 여부 (success, error)
+        msg `str`:
+            응답 메시지
+        history `list`:
+            대화 히스토리
+    """
+    # Error: 데이터 형식이 JSON이 아님
+    if not request.is_json:
+        return jsonify({
+            "result": "error", 
+            "msg": "missing json in request"
+        }), 400
         
     # 파라미터 받아오기
     user_msg = request.json["msg"] if "msg" in request.json else ""
     history = request.json["history"] if "history" in request.json != None else []
     
-    bot_msg, history = chatbot.chat(user_msg, history)
+    bot_msg, history = chatbot.rag(user_msg, history)
     
     return jsonify({
         "result": "success", 
